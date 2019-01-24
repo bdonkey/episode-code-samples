31f0dcd7b705a1b9b16e16999e451efc7f075d49
bdonkey <scott.schmidt1001@gmail.com>
2019-01-24 15:53:43 -0600

    old changes to commit

---
+++ 0023-zip-pt1/Zip.playground/Pages/ss01-episode.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+
+// public func zip<Sequence1, Sequence2>(
+//  _ sequence1: Sequence1,                  // A
+//  _ sequence2: Sequence2                   // B
+//  ) -> Zip2Sequence<Sequence1, Sequence2>  // (A, B)
+//  where Sequence1 : Sequence, Sequence2 : Sequence {
+//}
+"Wait a moment"
+Array(zip([1, 2, 3,4], ["one", "two", "three"]))
+let x: Int? = 5
+print("shit")
+
+print("shit1")
+print("hello")
---
+++ 0023-zip-pt1/Zip.playground/Pages/sstest.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+// test comment
+
+//: test
+/*:
+ * multiline comment
+ */
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
